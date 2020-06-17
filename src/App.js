d8ac495105df538f19a9835653847472b24c7428
Abdul Basit Abbasi <abbasi097@gmail.com>
2020-06-17 14:12:51 +0400

    Checking

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Abdul Basit
+          Learn React Abdul Basit Abbasi
         </a>
       </header>
     </div>
